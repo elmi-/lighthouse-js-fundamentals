8d7bd018ef1089401e6273f98fd5741560213047
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-10-14 08:20:20 +0000

    sayHello function

---
+++ hello.js
@@ -0,0 +1,12 @@
+//### Declaring and Calling Functions ###//
+//sayHello function
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+
+sayHello('Abdi');
